3337481eafdbfde5053c374897ff14beaefa8c7a
Flavio Granero <maltempe@gmail.com>
2016-04-27 15:31:43 -0300

    [#16] response_logger module wrapping Logger calls for responses

---
+++ lib/dayron/response_logger.ex
@@ -0,0 +1,16 @@
+defmodule Dayron.ResponseLogger do
+  @moduledoc """
+  Helper module wrapping Logger calls to register request/response events
+  """
+  require HTTPoison
+  require Logger
+
+  def log(method, url, _headers, _opts, %HTTPoison.Response{status_code: code}) do
+    Logger.debug [method, ?\s, url, ?\s, "-> #{code}"]
+  end
+
+  def log(method, url, _headers, _opts, %HTTPoison.Error{reason: reason}) do
+    Logger.error [method, ?\s, url, ?\s, "-> #{reason}"]
+  end
+
+end
