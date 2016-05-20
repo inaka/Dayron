5d1611d6d6800503f63d691686ef580004ae687c
Flavio Granero <maltempe@gmail.com>
2016-05-20 16:53:23 -0300

    fixing required poison version, bumo version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Dayron.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
@@ -43,7 +43,7 @@ defmodule Dayron.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:poison,       "~> 2.0"},
+      {:poison,       "~> 1.5 or ~> 2.0"},
       {:httpoison,    "~> 0.8.0"},
       {:crutches,     "~> 1.0.0"},
       {:credo,        "~> 0.3",     only: [:dev, :test]},
