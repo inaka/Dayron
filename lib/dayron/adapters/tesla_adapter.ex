05ab12cab8f8bcd4900936e2d75fcaf648f7d17e
Johan Wärlander <johan@snowflake.nu>
2016-10-04 23:15:41 +0200

    Make a working GET request with Tesla

---
+++ lib/dayron/adapters/tesla_adapter.ex
@@ -0,0 +1,66 @@
+defmodule Dayron.TeslaAdapter do
+  @moduledoc """
+  Makes http requests using Tesla library.
+  Use this adapter to make http requests to an external Rest API.
+
+  ## Example config
+      config :my_app, MyApp.Repo,
+        adapter: Dayron.TeslaAdapter,
+        url: "https://api.example.com"
+  """
+  @behaviour Dayron.Adapter
+
+  defmodule Client do
+    @moduledoc """
+    A Tesla Client implementation, sending json requests, parsing
+    json responses to Maps or a List of Maps. Maps keys are also converted to
+    atoms by default.
+    """
+    use Tesla
+
+    plug Tesla.Middleware.JSON
+
+    adapter Tesla.Adapter.Hackney
+  end
+
+  @doc """
+  Implementation for `Dayron.Adapter.get/3`.
+  """
+  def get(url, headers \\ [], opts \\ []) do
+    Client.get(url, headers: headers) |> translate_response
+  end
+
+  @doc """
+  Implementation for `Dayron.Adapter.post/4`.
+  """
+  def post(url, body, headers \\ [], opts \\ []) do
+    Client.post(url, body, headers: headers) |> translate_response
+  end
+
+  @doc """
+  Implementation for `Dayron.Adapter.patch/4`.
+  """
+  def patch(url, body, headers \\ [], opts \\ []) do
+    Client.patch(url, body, headers: headers) |> translate_response
+  end
+
+  @doc """
+  Implementation for `Dayron.Adapter.delete/3`.
+  """
+  def delete(url, headers \\ [], opts \\ []) do
+    Client.delete(url, headers: headers) |> translate_response
+  end
+
+  defp translate_response(%Tesla.Env{} = response) do
+    {:ok, %Dayron.Response{
+        status_code: response.status,
+        body: response.body |> Poison.decode! |> IO.inspect,
+        headers: response.headers |> Map.to_list
+      }
+    }
+  end
+  defp translate_response({:error, response}) do
+    data = response |> Map.from_struct
+    {:error, struct(Dayron.ClientError, data)}
+  end
+end
